a8e1a55d8d0d70740d2e1f0c042bb73c1eea681e
vx-clutch <owestness@gmail.com>
2025-09-23 19:23:43 -0400

    save

--- lib/err.c
+++ lib/err.c
@@ -15,124 +15,113 @@
 
 #define RESET "\e[0m"
 #define ERROR "\e[1;91m"
-#define WARN  "\e[1;95m"
-#define NOTE  "\e[1;94m"
-#define HINT  "\e[38;5;166m"
+#define WARN "\e[1;95m"
+#define NOTE "\e[1;94m"
+#define HINT "\e[38;5;166m"
 
-void
-errorf (const char *format, ...)
+void errorf(const char *format, ...)
 {
-  va_list args;
-  va_start (args, format);
-  fputs ("error: ", stderr);
-  vfprintf (stderr, format, args);
-  fputc ('\n', stderr);
-  va_end (args);
+	va_list args;
+	va_start(args, format);
+	fputs("error: ", stderr);
+	vfprintf(stderr, format, args);
+	fputc('\n', stderr);
+	va_end(args);
 }
 
-void
-fatalf (const char *format, ...)
+void fatalf(const char *format, ...)
 {
-  va_list args;
-  va_start (args, format);
-  fputs ("fatal: ", stderr);
-  vfprintf (stderr, format, args);
-  fputc ('\n', stderr);
-  va_end (args);
-  exit (EXIT_FAILURE);
+	va_list args;
+	va_start(args, format);
+	fputs("fatal: ", stderr);
+	vfprintf(stderr, format, args);
+	fputc('\n', stderr);
+	va_end(args);
+	exit(EXIT_FAILURE);
 }
 
-void
-notef (const char *format, ...)
+void notef(const char *format, ...)
 {
-  va_list args;
-  va_start (args, format);
-  fputs ("note: ", stderr);
-  vfprintf (stderr, format, args);
-  fputc ('\n', stderr);
-  va_end (args);
+	va_list args;
+	va_start(args, format);
+	fputs("note: ", stderr);
+	vfprintf(stderr, format, args);
+	fputc('\n', stderr);
+	va_end(args);
 }
 
-void
-warnf (const char *format, ...)
+void warnf(const char *format, ...)
 {
-  va_list args;
-  va_start (args, format);
-  fputs ("warning: ", stderr);
-  vfprintf (stderr, format, args);
-  fputc ('\n', stderr);
-  va_end (args);
+	va_list args;
+	va_start(args, format);
+	fputs("warning: ", stderr);
+	vfprintf(stderr, format, args);
+	fputc('\n', stderr);
+	va_end(args);
 }
 
-void
-hintf (const char *format, ...)
+void hintf(const char *format, ...)
 {
-  va_list args;
-  va_start (args, format);
-  fputs ("hint: ", stderr);
-  vfprintf (stderr, format, args);
-  fputc ('\n', stderr);
-  va_end (args);
+	va_list args;
+	va_start(args, format);
+	fputs("hint: ", stderr);
+	vfprintf(stderr, format, args);
+	fputc('\n', stderr);
+	va_end(args);
 }
 
-void
-errorfc (const char *format, ...)
+void errorfc(const char *format, ...)
 {
-  va_list args;
-  va_start (args, format);
-  fprintf (stderr, "%serror%s: ", ERROR, RESET);
-  vfprintf (stderr, format, args);
-  fputc ('\n', stderr);
-  va_end (args);
+	va_list args;
+	va_start(args, format);
+	fprintf(stderr, "%serror%s: ", ERROR, RESET);
+	vfprintf(stderr, format, args);
+	fputc('\n', stderr);
+	va_end(args);
 }
 
-void
-fatalfc (const char *format, ...)
+void fatalfc(const char *format, ...)
 {
-  va_list args;
-  va_start (args, format);
-  fprintf (stderr, "%sfatal%s: ", ERROR, RESET);
-  vfprintf (stderr, format, args);
-  fputc ('\n', stderr);
-  va_end (args);
-  exit (EXIT_FAILURE);
+	va_list args;
+	va_start(args, format);
+	fprintf(stderr, "%sfatal%s: ", ERROR, RESET);
+	vfprintf(stderr, format, args);
+	fputc('\n', stderr);
+	va_end(args);
+	exit(EXIT_FAILURE);
 }
 
-void
-notefc (const char *format, ...)
+void notefc(const char *format, ...)
 {
-  va_list args;
-  va_start (args, format);
-  fprintf (stderr, "%snote%s: ", NOTE, RESET);
-  vfprintf (stderr, format, args);
-  fputc ('\n', stderr);
-  va_end (args);
+	va_list args;
+	va_start(args, format);
+	fprintf(stderr, "%snote%s: ", NOTE, RESET);
+	vfprintf(stderr, format, args);
+	fputc('\n', stderr);
+	va_end(args);
 }
 
-void
-warnfc (const char *format, ...)
+void warnfc(const char *format, ...)
 {
-  va_list args;
-  va_start (args, format);
-  fprintf (stderr, "%swarning%s: ", WARN, RESET);
-  vfprintf (stderr, format, args);
-  fputc ('\n', stderr);
-  va_end (args);
+	va_list args;
+	va_start(args, format);
+	fprintf(stderr, "%swarning%s: ", WARN, RESET);
+	vfprintf(stderr, format, args);
+	fputc('\n', stderr);
+	va_end(args);
 }
 
-void
-hintfc (const char *format, ...)
+void hintfc(const char *format, ...)
 {
-  va_list args;
-  va_start (args, format);
-  fprintf (stderr, "%shint: ", HINT);
-  vfprintf (stderr, format, args);
-  fprintf (stderr, "\n%s", RESET);
-  va_end (args);
+	va_list args;
+	va_start(args, format);
+	fprintf(stderr, "%shint: ", HINT);
+	vfprintf(stderr, format, args);
+	fprintf(stderr, "\n%s", RESET);
+	va_end(args);
 }
 
-void
-error (int code)
+void error(int code)
 {
-  errorf (strerror (code));
+	errorf(strerror(code));
 }
