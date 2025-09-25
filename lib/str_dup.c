a99f3874d33b5d4699252c94df3375d7e0524ac2
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2025-09-25 13:12:49 -0400

    Update str_dup.c

--- lib/str_dup.c
+++ lib/str_dup.c
@@ -1,13 +1,12 @@
 #include <stdlib.h>
 #include <string.h>
+#include "xmem.h"
 
 #include "str_dup.h"
 
 char *str_dup(char *s)
 {
-	char *new = malloc(strlen(s) + 1);
-	if (!new)
-		return NULL;
+	char *new = xmalloc(strlen(s) + 1);
 	strcpy(new, s);
 	return new;
 }
